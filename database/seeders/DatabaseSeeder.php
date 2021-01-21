caccc3f4128becc3cdc90df68e25669453ea48d2
abdullahalharun <harun750@gmail.com>
2021-01-21 19:10:25 +0600

    react & inertia installation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Fleet Admin',
+            'email' => 'admin@fleethostbd.com',
+            'password' => \bcrypt('fleetbd321'),
+        ]);
+
     }
 }
---
+++ resources/js/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+ import React from 'react'
+
+ const HomePage = () => {
+     
+    return <p>Welcome to Home Page</p>;
+     
+ };
+
+ export default HomePage
